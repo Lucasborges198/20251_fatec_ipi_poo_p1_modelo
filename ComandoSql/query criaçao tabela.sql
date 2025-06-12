2b41254d00586e647130b55601328096faf0fae3
Lucas Borges Ribeiro <lucasborgesribeiro198@gmail.com>
2025-06-11 22:47:47 -0300

    alteraçao de txt para sql

--- ComandoSql/query criaçao tabela.sql
+++ ComandoSql/query criaçao tabela.sql
@@ -1,9 +1,9 @@
 create table TB_PERSONAGEM (
     ID int NOT NULL PRIMARY KEY,
     Nome VARCHAR(100) NOT NULL,
-    Prob_Construir NUMERIC(1, 3) NOT NULL,
-    Prob_Madeira NUMERIC(1, 3) NOT NULL,
-    Prob_Mineirar NUMERIC(1, 3) NOT NULL,
+    Prob_Construir NUMERIC(3, 2) NOT NULL,
+    Prob_Madeira NUMERIC(3, 2) NOT NULL,
+    Prob_Mineirar NUMERIC(3, 2) NOT NULL,
     Nr_Vitorias int,
     Nr_Derrotas int
 );
